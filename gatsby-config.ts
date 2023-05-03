227444e0391722cee5bcf3d16de85b0ceedf1e53
pantyetta <email@pantyetta.com>
2023-05-03 13:01:19 +0900

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,21 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Atrevete`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "aZ4hQHyfU5Zv_WDCnd-emMkZxFfsdq70VLi4f-O_2h8",
+      "spaceId": "olamdghkmjih"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-emotion"]
+};
+
+export default config;
