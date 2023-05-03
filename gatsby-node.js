ed47f3bec31b1a9fece8baca672bd66ee99dab88
pantyetta <email@pantyetta.com>
2023-05-04 01:21:25 +0900

    add event ^& posts pages

---
+++ gatsby-node.js
@@ -0,0 +1,43 @@
+const path = require(`path`)
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+    const PostTemplate = path.resolve(`src/templates/post.tsx`)
+    const EventTemplate = path.resolve(`src/templates/event.tsx`)
+    const result = await graphql(`
+        query {
+            allContentfulPost {
+                nodes {
+                    title
+                    contentful_id
+                }
+            }
+            allContentfulEvent {
+                nodes {
+                    title
+                    contentful_id
+                }
+            }
+        }
+    `)
+
+    result.data.allContentfulPost.nodes.forEach(node => {
+      createPage({
+            path: `post/${node.title}`,
+            component: PostTemplate,
+            context: {
+                contentful_id: node.contentful_id,
+            },
+        })
+    })
+
+    result.data.allContentfulEvent.nodes.forEach(node => {
+        createPage({
+            path: `event/${node.title}`,
+            component: EventTemplate,
+            context: {
+                contentful_id: node.contentful_id,
+            },
+        })
+    })
+  }
