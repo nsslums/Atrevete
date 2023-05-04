aee16d4e93e1ec9b0979091d842055e93a97ede3
pantyetta <email@pantyetta.com>
2023-05-04 13:19:55 +0900

    get event page content

--- src/templates/event.tsx
+++ src/templates/event.tsx
@@ -1,13 +1,36 @@
 import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import { HeadFC, PageProps, graphql } from "gatsby"
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
 
 
-const EventPage: React.FC<PageProps> = (props) => {
-  console.log(props.pageContext)
+const EventPage: React.FC<PageProps> = ({data}) => {
   return (
-    <p>{props.pageContext.contentful_id}</p>
+    <main>
+      <h1>{data.contentfulEvent.title}</h1>
+      <h2>概要</h2>
+      <div>{renderRichText(data.contentfulEvent.overview)}</div>
+      <h2>要項</h2>
+      <div>{renderRichText(data.contentfulEvent.requirements)}</div>
+      <h2>開催日</h2>
+      <p>{data.contentfulEvent.event_date}</p>
+    </main>
   )
 }
+
+export const query = graphql`
+  query($contentful_id: String!) {
+    contentfulEvent(contentful_id: {eq: $contentful_id}) {
+      title
+      event_date
+      overview {
+        raw
+      }
+      requirements {
+        raw
+      }
+    }
+  }
+`
 
 export default EventPage
 
