518cae92039716269540128ef97bbf4b84852851
pantyetta <email@pantyetta.com>
2023-05-04 13:20:06 +0900

    get post page content

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,13 +1,49 @@
 import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import { HeadFC, PageProps, graphql } from "gatsby"
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
 
 
-const PostPage: React.FC<PageProps> = (props) => {
-  console.log(props.pageContext)
-  return (
-    <p>{props.pageContext.contentful_id}</p>
-  )
-}
+const PostPage: React.FC<PageProps> = ({data}) => {
+    return (
+      <main>
+        <h1>{data.contentfulPost.title}</h1>
+        <h2>概要</h2>
+        <div>{renderRichText(data.contentfulPost.content)}</div>
+        <h2>タグ</h2>
+        <div>
+          {data.contentfulPost.metadata.tags.map(tag => (
+            <p>{tag.name}</p>
+          ))}
+        </div>
+        <h2>関連イベント</h2>
+        <div>
+          {data.contentfulPost.related_event.map(event => (
+            <p>{event.title}</p>
+          ))}
+        </div>
+      </main>
+    )
+  }
+  
+  export const query = graphql`
+    query ($contentful_id: String!) {
+      contentfulPost(contentful_id: {eq: $contentful_id}) {
+        title
+        content {
+          raw
+        }
+        metadata {
+          tags {
+            id
+          }
+        }
+        related_event {
+          contentful_id
+          title
+        }
+      }
+    }
+  `
 
 export default PostPage
 
