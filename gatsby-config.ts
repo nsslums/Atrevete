8c8867ab85bf86ce846fcae1f261ff91dbe94a58
pantyetta <email@pantyetta.com>
2023-05-06 01:21:08 +0900

    change contentful data for debug

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -13,8 +13,8 @@ const config: GatsbyConfig = {
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": process.env.accessToken,
-      "spaceId": process.env.spaceId,
+      "accessToken": process.env.DEBUG_ACCESSTOKEN,
+      "spaceId": process.env.DEBUG_SPACEID,
       enableTags: true,
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-emotion"]
