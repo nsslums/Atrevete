d7afd6fd6cea8ecb62fba7a10021b54e5ae99987
pantyetta <email@pantyetta.com>
2023-05-06 01:35:43 +0900

    rich text Enmed ^& assets

--- src/templates/event.tsx
+++ src/templates/event.tsx
@@ -1,16 +1,75 @@
 import * as React from "react"
 import { HeadFC, PageProps, graphql } from "gatsby"
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
-
+import { GatsbyImage } from "gatsby-plugin-image"
+import { BLOCKS, INLINES } from "@contentful/rich-text-types"
 
 const EventPage: React.FC<PageProps> = ({data}) => {
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: ({data}) => {
+        const { gatsbyImageData } = data.target
+        if (!gatsbyImageData) {
+          // asset is not an image
+          return null
+        }
+        return <GatsbyImage image={gatsbyImageData} alt={""} />
+      },
+      [BLOCKS.EMBEDDED_ENTRY]: ({data}) =>{
+        let link;
+        if(data.target.__typename == "ContentfulEvent")
+          link = `/event/${data.target.title}`
+        else if(data.target.__typename == "ContentfulPost")
+          link = `/post/${data.target.title}`
+        else
+          return null
+        return  (
+          <div>
+            <Link to={link}>{data.target.title}</Link>
+          </div>
+        )
+      },
+      [INLINES.ENTRY_HYPERLINK]: ({data}) =>{
+        let link;
+        if(data.target.__typename == "ContentfulEvent")
+          link = `/event/${data.target.title}`
+        else if(data.target.__typename == "ContentfulPost")
+          link = `/post/${data.target.title}`
+        else
+          return null
+        return  (
+          <div>
+            <Link to={link}>{data.target.title}</Link>
+          </div>
+        )
+      },
+      [INLINES.ASSET_HYPERLINK]: ({data}) =>{
+        return  (
+          <a href={data.target.url} target="_blank" rel="noopener noreferrer nofollow">{data.target.title}</a>
+        )
+      },[INLINES.EMBEDDED_ENTRY]: ({data}) =>{
+        let link;
+        if(data.target.__typename == "ContentfulEvent")
+          link = `/event/${data.target.title}`
+        else if(data.target.__typename == "ContentfulPost")
+          link = `/post/${data.target.title}`
+        else
+          return null
+        return  (
+            <Link to={link}>{data.target.title}</Link>
+        )
+      }
+      ,
+    },
+  }
+
   return (
     <main>
       <h1>{data.contentfulEvent.title}</h1>
       <h2>概要</h2>
-      <div>{renderRichText(data.contentfulEvent.overview)}</div>
+      <div>{renderRichText(data.contentfulEvent.overview, options)}</div>
       <h2>要項</h2>
-      <div>{renderRichText(data.contentfulEvent.requirements)}</div>
+      <div>{renderRichText(data.contentfulEvent.requirements, options)}</div>
       <h2>開催日</h2>
       <p>{data.contentfulEvent.event_date}</p>
     </main>
@@ -24,9 +83,47 @@ export const query = graphql`
       event_date
       overview {
         raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            __typename
+            gatsbyImageData
+            url
+            title
+          }
+          ... on ContentfulEvent {
+            contentful_id
+            __typename
+            title
+          }
+          ... on ContentfulPost {
+            contentful_id
+            __typename
+            title
+          }
+        }
       }
       requirements {
         raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            __typename
+            gatsbyImageData
+            url
+            title
+          }
+          ... on ContentfulEvent {
+            contentful_id
+            __typename
+            title
+          }
+          ... on ContentfulPost {
+            contentful_id
+            __typename
+            title
+          }
+        }
       }
     }
   }
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,36 +1,115 @@
 import * as React from "react"
-import { HeadFC, PageProps, graphql } from "gatsby"
+import { HeadFC, Link, PageProps, graphql } from "gatsby"
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import { GatsbyImage } from "gatsby-plugin-image"
+import { BLOCKS, INLINES } from "@contentful/rich-text-types"
 
+const PostPage: React.FC<PageProps> = ({ data }) => {
 
-const PostPage: React.FC<PageProps> = ({data}) => {
-    return (
-      <main>
-        <h1>{data.contentfulPost.title}</h1>
-        <h2>概要</h2>
-        <div>{renderRichText(data.contentfulPost.content)}</div>
-        <h2>タグ</h2>
-        <div>
-          {data.contentfulPost.metadata.tags?.map(tag => (
-            <p key={tag.contentful_id}>{tag.name}</p>
-          ))}
-        </div>
-        <h2>関連イベント</h2>
-        <div>
-          {data.contentfulPost.related_event?.map(event => (
-            <p key={event.contentful_id}>{event.title}</p>
-          ))}
-        </div>
-      </main>
-    )
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: ({data}) => {
+        const { gatsbyImageData } = data.target
+        if (!gatsbyImageData) {
+          // asset is not an image
+          return null
+        }
+        return <GatsbyImage image={gatsbyImageData} alt={""} />
+      },
+      [BLOCKS.EMBEDDED_ENTRY]: ({data}) =>{
+        let link;
+        if(data.target.__typename == "ContentfulEvent")
+          link = `/event/${data.target.title}`
+        else if(data.target.__typename == "ContentfulPost")
+          link = `/post/${data.target.title}`
+        else
+          return null
+        return  (
+          <div>
+            <Link to={link}>{data.target.title}</Link>
+          </div>
+        )
+      },
+      [INLINES.ENTRY_HYPERLINK]: ({data}) =>{
+        let link;
+        if(data.target.__typename == "ContentfulEvent")
+          link = `/event/${data.target.title}`
+        else if(data.target.__typename == "ContentfulPost")
+          link = `/post/${data.target.title}`
+        else
+          return null
+        return  (
+          <div>
+            <Link to={link}>{data.target.title}</Link>
+          </div>
+        )
+      },
+      [INLINES.ASSET_HYPERLINK]: ({data}) =>{
+        return  (
+          <a href={data.target.url} target="_blank" rel="noopener noreferrer nofollow">{data.target.title}</a>
+        )
+      },[INLINES.EMBEDDED_ENTRY]: ({data}) =>{
+        let link;
+        if(data.target.__typename == "ContentfulEvent")
+          link = `/event/${data.target.title}`
+        else if(data.target.__typename == "ContentfulPost")
+          link = `/post/${data.target.title}`
+        else
+          return null
+        return  (
+            <Link to={link}>{data.target.title}</Link>
+        )
+      }
+      ,
+    },
   }
-  
-  export const query = graphql`
+
+  return (
+    <main>
+      <h1>{data.contentfulPost.title}</h1>
+      <h2>内容</h2>
+      <div>{renderRichText(data.contentfulPost.content, options)}</div>
+      <h2>タグ</h2>
+      <div>
+        {data.contentfulPost.metadata.tags?.map(tag => (
+          <p key={tag.contentful_id}>{tag.name}</p>
+        ))}
+      </div>
+      <h2>関連イベント</h2>
+      <div>
+        {data.contentfulPost.related_event?.map(event => (
+          <Link to={'/event/' + event.title} key={event.contentful_id}>{event.title}</Link>
+        ))}
+      </div>
+    </main>
+  )
+}
+
+export const query = graphql`
     query ($contentful_id: String!) {
       contentfulPost(contentful_id: {eq: $contentful_id}) {
         title
         content {
           raw
+          references {
+            ... on ContentfulAsset {
+              contentful_id
+              __typename
+              gatsbyImageData
+              url
+              title
+            }
+            ... on ContentfulEvent {
+              contentful_id
+              __typename
+              title
+            }
+            ... on ContentfulPost {
+              contentful_id
+              __typename
+              title
+            }
+          }
         }
         metadata {
           tags {
