8fc22882c4b1a78bb22ef013aaaa74898a7dd71c
pantyetta <email@pantyetta.com>
2023-05-06 01:22:00 +0900

    change enviroment name for send email api

--- src/api/form.js
+++ src/api/form.js
@@ -10,8 +10,8 @@ export default function formHandler(req, res){
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-          user: process.env.smtpUser,
-          pass: process.env.smtpPass,
+          user: process.env.SMTPUSER,
+          pass: process.env.SMTPPASS,
         },
     });
 
