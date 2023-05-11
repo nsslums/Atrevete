38a4ae9cc38e011a637f4d4b663a7ac60df5dfd7
pantyetta <email@pantyetta.com>
2023-05-12 00:47:03 +0900

    show Events slider on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,26 @@
 import * as React from "react"
-import { HeadFC, PageProps, graphql } from "gatsby"
+import { HeadFC, Link, PageProps, graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 import ModalSearch from '../components/modalsearch';
+import { EventCard } from "../stories/atrevete/EventCard";
+import GlobalStyle from "../GlobalStyle";
+import { Events } from "../components/events";
 
 const IndexPage: React.FC<PageProps> = ({data}) => {
   return (
-    <div>
-      <h2>協賛企業</h2>
-      <ul>
-        {data.allContentfulSponsor.nodes?.map(sponsor => (
-          <a href={sponsor.url} target="_blank" rel="noopener noreferrer"><GatsbyImage image={sponsor.logo.gatsbyImageData} alt={sponsor.name} /></a>
-        ))}
-      </ul>
-      <ModalSearch />
-    </div>
+    <>
+      <GlobalStyle />
+      <div>
+        <h2>協賛企業</h2>
+        <ul>
+          {data.allContentfulSponsor.nodes?.map(sponsor => (
+            <a href={sponsor.url} target="_blank" rel="noopener noreferrer"><GatsbyImage image={sponsor.logo.gatsbyImageData} alt={sponsor.name} /></a>
+          ))}
+        </ul>
+        <ModalSearch />
+        <Events/>
+      </div>
+    </>
   )
 }
 
