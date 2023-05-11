5d49b246eaf2d3847dd0e515422ab807b5dbc9e4
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2023-05-12 00:56:02 +0900

    kansei simasenn desitaaaaaaaaaaa

--- src/stories/atrevete/GoldButton.stories.ts
+++ src/stories/atrevete/GoldButton.stories.ts
@@ -14,5 +14,7 @@ type Story = StoryObj<typeof meta>;
 
 
 export const Default: Story = {
+  args: {
+  }
  
 }
--- src/stories/atrevete/GoldButton.tsx
+++ src/stories/atrevete/GoldButton.tsx
@@ -1,17 +1,36 @@
 import { css } from '@emotion/react';
 import React from 'react';
-import { GatsbyImage,  IGatsbyImageData } from 'gatsby-plugin-image';
 
 interface ButtonProps {
   text: string;
+  onClick?: () => void;
 }
+
+const Style = css`
+  color: white;
+  background: linear-gradient(20deg,#C5B286,#6A3B05);
+  padding: 20px 60px;
+  border-radius: 3px;
+  &::before {
+    content: "";
+    background-color: blue;
+    width: 100px;
+    height: 100px;
+  }
+  &:hover {
+    background: orange;
+    box-shadow: 0 0 20px 0 black;
+    border: solid 1px rgba(255,255,255,10);
+  }
+`
+
 
 export const GoldButton = ({
   text = 'Button',
   ...props 
 }: ButtonProps) => {
   return (
-    <button>button</button>
+    <button type='button' css={Style} {...props}>{text}</button>
 
-    )
-};
+  )
+}
