db98445d0ccb8a066371ef10c2aece174f4631b6
pantyetta <email@pantyetta.com>
2023-05-14 00:47:55 +0900

    storybook header

---
+++ src/stories/atrevete/Header.tsx
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+import React from 'react';
+
+interface HeaderProps {
+}
+
+export const Header = ({
+    
+}: HeaderProps) =>{
+    return(
+        <header css={{
+            width: "100vw"
+        }}>
+            logo
+        </header>
+    )
+}
