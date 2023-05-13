714e1d26888737eca609a764236eb60a70ef2a17
pantyetta <email@pantyetta.com>
2023-05-14 00:47:48 +0900

    storybook create footer

---
+++ src/stories/atrevete/Footer.tsx
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+import React from 'react';
+
+interface FooterProps {
+}
+
+export const Footer = ({
+    
+}: FooterProps) =>{
+    return(
+        <footer css={{
+            width: "100vw"
+        }}>
+            logo
+        </footer>
+    )
+}
