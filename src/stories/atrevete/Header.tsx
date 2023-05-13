5590a93875f39108f81622515c412cb867715b5e
pantyetta <email@pantyetta.com>
2023-05-14 01:40:25 +0900

    menu close header

--- src/stories/atrevete/Header.tsx
+++ src/stories/atrevete/Header.tsx
@@ -1,17 +1,51 @@
 import { css } from '@emotion/react';
-import React from 'react';
+import React, { useState } from 'react';
+import {CgMenuRightAlt} from "@react-icons/all-files/cg/CgMenuRightAlt"
 
 interface HeaderProps {
 }
 
 export const Header = ({
-    
+
 }: HeaderProps) =>{
+
+    const [openMenu, setOpenMenu] = React.useState(false)
+
+    const onClick = () =>{
+        setOpenMenu(!openMenu)
+    }
+
     return(
         <header css={{
-            width: "100vw"
+            width: "100%",
+            position: "relative",
+            top: 0,
+            left: 0,
         }}>
-            logo
+            <div css={{
+                height: "98px",
+                width: "95%",
+                margin: "auto",
+                display: "flex",
+                justifyContent: 'space-between',
+                alignItems: 'center',
+            }}>
+                <div css={{
+
+                }}>
+                    logo
+                </div>
+                <div css={{
+                        width: "30px",
+                        height: "30px",
+                        cursor: "pointer",
+                    }} onClick={onClick}>
+                    <CgMenuRightAlt css={{
+                        width: "30px",
+                        height: "30px",
+                    }} />
+                </div>
+            </div>
         </header>
     )
 }
