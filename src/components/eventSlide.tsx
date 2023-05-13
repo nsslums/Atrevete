313e1bc3ccace7d5d521f36e7186a93d5d9f2930
pantyetta <email@pantyetta.com>
2023-05-13 14:33:33 +0900

    rename events slider & slide EventCard

---
+++ src/components/eventSlide.tsx
@@ -0,0 +1,124 @@
+import { graphql, useStaticQuery } from "gatsby";
+import React from "react";
+import Slider from "react-slick"
+import { EventCard } from "../stories/atrevete/EventCard";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import {FaChevronLeft} from "@react-icons/all-files/fa/FaChevronLeft"
+import {FaChevronRight} from "@react-icons/all-files/fa/FaChevronRight"
+
+
+const arrow_css = css({
+    position: "absolute",
+    width: "40px",
+    height: "40px",
+    top: "50%",
+    transform: "translate(0, -50%)",
+    backgroundColor: "rgba(255,255,255, .4)",
+    borderRadius: "90px",
+    cursor: "pointer",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#1E1E1E",
+    zIndex: 20,
+})
+
+const sleeveCurtain = css({
+    "&::before": {
+        content: '""',
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "50px",
+        height: "100%",
+        background: "linear-gradient(90deg, #121212 0%, rgba(18, 18, 18, 0.713542) 47.4%, rgba(18, 18, 18, 0) 100%)",
+        zIndex: 10
+    },
+
+    "&::after": {
+        content: '""',
+        position: "absolute",
+        top: 0,
+        right: 0,
+        width: "50px",
+        height: "100%",
+        background: "linear-gradient(-90deg, #121212 0%, rgba(18, 18, 18, 0.713542) 47.4%, rgba(18, 18, 18, 0) 100%)",
+        zIndex: 10
+    }
+})
+
+function NextArrow(props: any) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        css={[arrow_css, { right: "-20px",}]}
+        onClick={onClick}
+      >
+        <FaChevronRight />
+    </div>
+    );
+  }
+
+  function PrevArrow(props: any) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        css={[arrow_css, { left: "-20px",}]}
+        onClick={onClick}
+      >
+        <FaChevronLeft />
+    </div>
+    );
+  }
+  
+
+export const Events: React.FC = () => {
+
+    const result = useStaticQuery(graphql`
+        query{
+            allContentfulEvent(filter: {hidden: {ne: true}}) {
+                nodes {
+                    title
+                    contentful_id,
+                    date,
+                    eye_catch {
+                        gatsbyImageData
+                    }
+                }
+            }
+        }
+    `)
+
+    const settings = {
+        infinite: true,
+        speed: 500,
+        dots: false,
+        arrows: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: "50px",
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+    }
+
+    const Contener = styled.div({
+        width: "980px",
+        margin: "auto",
+    })
+
+    return (
+    <Contener>
+         <div>
+             <Slider {...settings} css={sleeveCurtain}>
+                 {result.allContentfulEvent.nodes?.map((event: any) => (
+                    <div><EventCard key={event.contentful_id} title={event.title} url={"/event/" + event.title} date={event.date} image={event.eye_catch}/></div>
+                 ))}
+             </Slider>
+         </div>
+    </Contener>
+    )
+}
