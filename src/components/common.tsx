63a207a17507d3e52f583eb8faa120bc0c3e2795
pantyetta <email@pantyetta.com>
2023-05-14 01:39:09 +0900

    gatsby common componets

---
+++ src/components/common.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "../stories/atrevete/Header";
+import { Footer } from "../stories/atrevete/Footer";
+import GlobalStyle from "../GlobalStyle";
+
+export const Common = ({children}: any) => {
+    return(
+        <>
+            <Header/>
+            <GlobalStyle/>
+            {children}
+            <Footer/>
+        </>
+    )
+}
