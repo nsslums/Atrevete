d460aad71562a58295010dba8968b0a5ed128a97
pantyetta <email@pantyetta.com>
2023-05-14 23:21:51 +0900

    matrix head3

--- src/stories/atrevete/Head3.tsx
+++ src/stories/atrevete/Head3.tsx
@@ -7,15 +7,27 @@ interface Head3Props {
 
 const Style = css({
   position: 'relative',
-  background: "linear-gradient(264.42deg, rgba(106, 59, 5, 0.89) 0%, rgba(106, 59, 5, 0.54) 61.46%, rgba(106, 59, 5, 0.37) 100%), #C5B286",
   padding: "5px 10px",
-  maxWidth: "125px",
+  minWidth: "125px",
   textAlign: 'center',
+  display: 'inline-block',
+
+  "&::after": {
+    content: '""',
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    background: "linear-gradient(264.42deg, rgba(106, 59, 5, 0.89) 0%, rgba(106, 59, 5, 0.54) 61.46%, rgba(106, 59, 5, 0.37) 100%), #C5B286",
+    transform: "matrix(1, 0, -0.25, 0.97, 0, 0)",
+    zIndex: 0,
+  }
 })
 
 
 export const Head3 = ({
   text,
 }: Head3Props) => {
-  return  <h3 css={Style}>{text}</h3>
+  return <h3 css={Style} className='bg_noise'><div css={{zIndex: 10, position: "relative"}}>{text}</div></h3>
 }
