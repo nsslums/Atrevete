d8cc932c99a8f2ab68873d2bdfa22dd3f4508b3f
pantyetta <email@pantyetta.com>
2023-05-14 22:24:24 +0900

    change Globalstyle index

--- src/components/common.tsx
+++ src/components/common.tsx
@@ -6,8 +6,8 @@ import GlobalStyle from "../GlobalStyle";
 export const Common = ({children}: any) => {
     return(
         <>
-            <Header/>
             <GlobalStyle/>
+            <Header/>
             {children}
             <Footer/>
         </>
