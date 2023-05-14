29b4c061a2a0b654983a3df76c846d13395d1e4d
pantyetta <email@pantyetta.com>
2023-05-14 16:41:29 +0900

    code index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,85 @@
 import * as React from "react"
 import { HeadFC, Link, PageProps, graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
 import ModalSearch from '../components/modalsearch';
 import { Events } from "../components/eventSlide";
 import {Common} from "../components/common"
 import { GoldButton } from "../stories/atrevete/GoldButton";
+import { css } from "@emotion/react";
+import { Head1 } from "../stories/atrevete/Head1";
+
+
+const topImage = css({
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+})
+
+const topPhrase = css({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  zIndex: 10,
+})
+
+const categoryBlock = css({
+  width: "100%",
+  height: "60vh",
+  display:"flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  position: "relative"
+})
 
 const IndexPage: React.FC<PageProps> = ({data}) => {
   return (
     <Common>
-      <div>
-        <h2>協賛企業</h2>
-        <ul>
-          {data.allContentfulSponsor.nodes?.map(sponsor => (
-            <a href={sponsor.url} target="_blank" rel="noopener noreferrer"><GatsbyImage image={sponsor.logo.gatsbyImageData} alt={sponsor.name} /></a>
-          ))}
-        </ul>
+      <>
+        <div css={{
+          width: "100%",
+          height: "100vh",
+          position: "relative",
+          top: "-98px",
+          zIndex: 0
+        }}>
+          <StaticImage src="../images/top.jpg" alt="TopImage" css={topImage}/>
+          <div css={topPhrase}>
+            <p css={{fontSize: "40px", fontWeight: 700}}>未来と今をつなぐ</p>
+          </div>
+        </div>
+        <div css={{height: "80vh", display:"flex", flexDirection: "column", alignItems: "center", justifyContent: "center", position: "relative"}}>
+          <p css={{display: "flex", flexDirection: "column", alignItems: "center", fontSize: "40px", fontWeight: 700, lineHeight: "66px"}}>
+            <span>吾輩はネコである。名前はまだない。</span>
+            <span>どこで生まれたかとんと見当がつかぬ。</span>
+          </p>
+          <div css={{marginTop: "5%"}}><GoldButton text="More"/></div>
+        </div>
+        <div css={categoryBlock}>
+          <div><Head1 text="イベント"/></div>
+          <Events/>
+        </div>
+        <div css={categoryBlock}>
+          <div><Head1 text="ニュース"/></div>
+          <div></div>
+        </div>
+        <div css={categoryBlock}>
+          <div><Head1 text="協賛企業"/></div>
+          <ul>
+            {data.allContentfulSponsor.nodes?.map(sponsor => (
+              <a href={sponsor.url} target="_blank" rel="noopener noreferrer"><GatsbyImage image={sponsor.logo.gatsbyImageData} alt={sponsor.name} /></a>
+            ))}
+          </ul>
+        </div>
+        
         <ModalSearch />
-        <Events/>
-        <GoldButton />
-      </div>
+
+      </>
     </Common>
   )
 }
