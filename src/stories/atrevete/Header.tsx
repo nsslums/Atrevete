c4457f9766f865f22e12c706f8195c48f6f6f020
pantyetta <email@pantyetta.com>
2023-05-18 00:23:33 +0900

    add Header hamburger Menu

--- src/stories/atrevete/Header.tsx
+++ src/stories/atrevete/Header.tsx
@@ -2,15 +2,32 @@ import { css } from '@emotion/react';
 import React, { useState } from 'react';
 import {CgMenuRightAlt} from "@react-icons/all-files/cg/CgMenuRightAlt"
 import { StaticImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby-link';
 
 interface HeaderProps {
+    menuOpen?: boolean
 }
 
-export const Header = ({
+const menu = css({
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "#1E1E1E;"
+})
 
+const block = css({
+    fontSize: 40,
+    padding: "5px 0",
+    marginRight: "1em"
+})
+
+export const Header = ({
+    menuOpen = false
 }: HeaderProps) =>{
 
-    const [openMenu, setOpenMenu] = React.useState(false)
+    const [openMenu, setOpenMenu] = React.useState(menuOpen)
 
     const onClick = () =>{
         setOpenMenu(!openMenu)
@@ -48,6 +65,29 @@ export const Header = ({
                     }} />
                 </div>
             </div>
+            {openMenu ? <div css={menu}>
+                <div css={{
+                    display: 'flex',
+                    flexDirection: "column",
+                    alignItems: 'end',
+                }}>
+                    <div css={{
+                            width: "30px",
+                            height: "30px",
+                            cursor: "pointer",
+                        }} onClick={onClick}>
+                            <CgMenuRightAlt css={{
+                            width: "30px",
+                            height: "30px",
+                        }} />
+                    </div>
+                    <div css={block}><Link to='/'>トップページ</Link></div>
+                    <div css={block}><Link to='/about'>Atreveteとは</Link></div>
+                    <div css={block}><Link to='/post'>投稿</Link></div>
+                    <div css={block}><Link to='/contact'>お問い合わせ</Link></div>
+                </div>
+            </div>
+            : false }
         </header>
     )
 }
