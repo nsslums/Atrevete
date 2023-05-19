39ae2dbdbc2f85f2e0cd015682163bdbec25781e
pantyetta <email@pantyetta.com>
2023-05-19 15:34:20 +0900

    change contact api use contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,24 +1,27 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import { Common } from "../components/common"
+import { Input } from "../stories/atrevete/form/Input"
+import { TextArea } from "../stories/atrevete/form/TextArea"
 
 const FormPage: React.FC<PageProps> = () => {
-    const [formValue, setFormValue] = React.useState({name: '', email: '', phone: '', subject: '' ,content: ''})
     const [submitdis, setSubmitdis] = React.useState(false)
 
-    const changeHandle = (e) =>{
-        console.log(formValue)
-        setFormValue({...formValue, [e.target.id]: e.target.value})
-    }
-
-    const onSubmit = async (e) =>{
+    const onSubmit = async (e: any) =>{
+        console.log("submit")
         setSubmitdis(true)
         e.preventDefault()  // デフォルトの動作のキャンセル
+
+        const formData = new FormData()
+        formData.append("name", e.target.name.value,)
+        formData.append("email", e.target.email.value)
+        formData.append("subject", e.target.subject.value)
+        formData.append("phone", e.target.phone.value)
+        formData.append("content", e.target.content.value)
+
         const response = await window.fetch('/api/contact', {
             method: 'POST',
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify(formValue),
+            body: formData,
         })
         .then(res => res.json())
         console.log(response)
@@ -26,31 +29,16 @@ const FormPage: React.FC<PageProps> = () => {
     }
 
     return (
-        <form action="/api/contact" method="post" onSubmit={onSubmit}>
-            <div>
-                <label htmlFor="name">name</label>
-                <input type="name" name="name" id="name" value={formValue.name} onChange={changeHandle} required/>
-            </div>
-            <div>
-                <label htmlFor="email">email</label>
-                <input type="email" name="email" id="email" value={formValue.email} onChange={changeHandle} required/>
-            </div>
-            <div>
-                <label htmlFor="phone">phone</label>
-                <input type="tel" name="phone" id="phone" value={formValue.phone} onChange={changeHandle}/>
-            </div>
-            <div>
-                <label htmlFor="content">subject</label>
-                <input type="text" name="subject" id="subject" value={formValue.subject} onChange={changeHandle} required/>
-            </div>
-            <div>
-                <label htmlFor="subject">content</label>
-                <textarea name="content" id="content" value={formValue.content} onChange={changeHandle} required/>
-            </div>
-            <div>
-                <input type="submit" disabled={submitdis}/>
-            </div>
-        </form>
+        <Common>
+            <form action="/api/contact" method="post" onSubmit={onSubmit}>
+                <Input label="お名前" type="text" name="name" id="name" required={true} />
+                <Input label="メール" type="email" name="email" id="email" required={true} />
+                <Input label="件名" type="text" name="subject" id="subject" required={true} />
+                <Input label="電話" type="tel" name="phone" id="phone"/>
+                <TextArea label="ご内容" name="content" id="content" required={true} />
+                <Input type="submit" name="submit" id="submit" disabled={submitdis} />
+            </form>
+        </Common>
     )
 }
 
