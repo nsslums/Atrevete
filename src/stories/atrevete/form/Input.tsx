e513d68d3bb42b1f8567971ebe8d8a6560f7da7c
ari-yuto <115969520+ari-yuto@users.noreply.github.com>
2023-05-22 15:19:05 +0900

    Input css

--- src/stories/atrevete/form/Input.tsx
+++ src/stories/atrevete/form/Input.tsx
@@ -1,5 +1,4 @@
 import { css } from '@emotion/react';
-import styled from '@emotion/styled';
 import React from 'react';
 
 interface inputProps {
@@ -11,8 +10,34 @@ interface inputProps {
   disabled?: boolean,
 }
 
-const Style = css({
-    borderBottom: "1px solid white"
+const rootCss = css({
+    position: "relative",
+    fontSize: 16
+})
+
+
+const labelBase = css({
+  position: "absolute",
+  top: 35,
+  left: 5,
+  transition: "0.4s cubic-bezier(0,.5,.5,1)",
+  fontSize: "1em",
+})
+const InputCss = ({
+  width: 700,
+  height: 30,
+  borderBottom: "1px white solid",
+  padding: "7px 10px",
+  marginTop: 30,
+  backgroundColor: "#202020",
+  borderRadius: "5px 5px 0 0"
+})
+
+
+const label_up = css({
+  top: 5,
+  left: 5,
+  fonrSize: "0.8em",
 })
 
 export const Input = ({
@@ -24,19 +49,30 @@ export const Input = ({
     disabled = false,
 }: inputProps) => {
   const [value, setValue] = React.useState("")
+  const [isSelect, setIsSelect] = React.useState(false)
 
   const changeHandle = (e: any) =>{
     setValue(e.target.value)
   }
 
+  const onSelectHandel = (e: any) =>{
+    setIsSelect(true)
+  }
+
+  const onBlurHandel = (e: any) =>{
+    setIsSelect(false)
+  }
+
+  const labelStyle = isSelect || value ? [labelBase, label_up] :labelBase
+
   return (
-    <div>
-      {label ? <label htmlFor={id}>{label}</label> : false}
+    <div css={rootCss}>
+      {label ? <label htmlFor={id} css={labelStyle}>{label}</label> : false}
       {type == 'submit' ? 
         <input type="submit" disabled={disabled}/>
-      :  
-      <input type={type} name={name} id={id} required={required} css={Style} value={value} onChange={changeHandle}/>
-    }
+        :  
+        <input type={type} name={name} id={id} value={value} required={required} css={InputCss} onBlur={onBlurHandel} onSelect={onSelectHandel} onChange={changeHandle} autoComplete="off"/>
+      }
     </div>
   )
 }
