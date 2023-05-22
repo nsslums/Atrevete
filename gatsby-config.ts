d0a39f0f6d03d2d0baa6ae36f8f1a9da3883bafc
pantyetta <email@pantyetta.com>
2023-05-22 09:32:19 +0900

    add robots.txt canoicalUrl pllugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Atrevete`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://testatrevete.netlify.app`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -85,7 +85,12 @@ const config: GatsbyConfig = {
         }
       },
     },
-  }]
+  }, 'gatsby-plugin-robots-txt',{
+    resolve: `gatsby-plugin-canonical-urls`,
+    options: {
+      siteUrl: `https://testatrevete.netlify.app`,
+    },
+  },]
 };
 
 export default config;
