aafb471a0260dcb757468d5b89a75de6a61ca593
pantyetta <email@pantyetta.com>
2023-05-22 14:56:44 +0900

    pulldown css

--- src/stories/atrevete/form/Pulldown.tsx
+++ src/stories/atrevete/form/Pulldown.tsx
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
 import React from 'react';
 import Select from 'react-select';
+import './Pulldown.css'
 
 interface PulldownProps {
   label?: string,
@@ -11,22 +12,65 @@ interface PulldownProps {
   default_val?:any,
 }
 
-const Style = css({
+const rootCss = css({
+  position: "relative",
+  fontSize: 16
+})
 
+const labelBase = css({
+  position: "absolute",
+  top: 36,
+  left: 10,
+  transition: "0.4s cubic-bezier(0,.5,.5,1)",
+  fontSize: "1em",
+  zIndex: 10,
+})
+
+const label_up = css({
+  top: 5,
+  left: 5,
+  fontSize: ".85em",
+})
+
+const pullDownCss = css({
+  width: 700,
+  marginTop: 30,
+  backgroundColor: "#202020",
+  borderRadius: "5px 5px 0 0",
+  display: "inline-block",
 })
 
 export const Pulldown = ({
     label,
-    options = [{ value: 'value1', label: '値1'}],
+    options = [{ value: 'value1', label: '値1'},{ value: 'value2', label: '値2'},],
     name,
     id,
     required = false,
     default_val,
 }: PulldownProps) => {
+  const [isSelect, setIsSelect] = React.useState(false)
+  const [value, setValue] = React.useState(default_val)
+
+  const onBlurHandle = (e: any) =>{
+    setIsSelect(false)
+  }
+
+  const menuOpenHandle = () =>{
+    setIsSelect(true)
+  }
+
+  const changeHandole = (e: any) =>{
+    setValue(e)
+  }
+
+  const labelStyle =  isSelect || value ? [labelBase, label_up] :labelBase
+
   return (
-    <div>
-      {label ? <label htmlFor={id}>{label}</label> : false}
-      <Select options={options} name={name} id={id} required={required} defaultValue={default_val} css={Style}/>
+    <div css={rootCss}>
+      {label ? <label htmlFor={id} css={labelStyle}>{label}</label> : false}
+      <Select options={options} name={name} id={id} required={required} defaultValue={default_val} css={pullDownCss} classNamePrefix="react-select" placeholder='' components={{IndicatorSeparator: () => null,}}
+        onBlur={onBlurHandle} onMenuOpen={menuOpenHandle} onChange={changeHandole}
+      />
     </div>
   )
 }
