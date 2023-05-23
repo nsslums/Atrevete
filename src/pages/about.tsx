a4ebba13e6d6ab9fba05affa3fb86bc36c4301c6
pantyetta <email@pantyetta.com>
2023-05-23 14:58:56 +0900

    create about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { HeadFC, Link, PageProps, graphql } from "gatsby"
+import { Common } from "../components/common"
+
+
+const Aboutage: React.FC<PageProps> = () => {
+
+    return (
+        <Common>
+        </Common>
+    )
+}
+
+export default Aboutage
+
+export const Head: HeadFC = () => <title>Event title</title>
