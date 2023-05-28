f7c1218104036012c5b1b9c4b4742fb468f0f455
pantyetta <email@pantyetta.com>
2023-05-28 22:01:41 +0900

    fix eventHead args

--- src/stories/atrevete/event/EventHead.stories.ts
+++ src/stories/atrevete/event/EventHead.stories.ts
@@ -13,12 +13,14 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     title: 'イベント名',
+    date: "2030/01/01"
   },
 };
 
 export const End: Story = {
   args: {
     title: 'イベント名',
+    date: "2020/01/01",
     isActive: false,
   },
 };
