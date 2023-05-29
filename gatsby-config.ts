98d5fd18993de8f320f259d3f712a69e5b56f9d2
pantyetta <email@pantyetta.com>
2023-05-29 10:45:13 +0900

    fix url => SITEURL

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Atrevete`,
-    siteUrl: process.env.URL
+    siteUrl: process.env.SITEURL
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -88,7 +88,7 @@ const config: GatsbyConfig = {
   }, 'gatsby-plugin-robots-txt',{
     resolve: `gatsby-plugin-canonical-urls`,
     options: {
-      siteUrl: process.env.URL,
+      siteUrl: process.env.SITEURL,
     },
   },]
 };
