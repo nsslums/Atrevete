645ec5a0015c3ad46cfa71d87a48c13ea4160a30
pantyetta <email@pantyetta.com>
2023-05-31 22:36:04 +0900

    posts page css

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -4,7 +4,28 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import { BLOCKS, INLINES } from "@contentful/rich-text-types"
 import { Common } from "../components/common"
 import { PostCard } from "../stories/atrevete/event/PostCard"
+import { css } from "@emotion/react"
+import { Head1 } from "../stories/atrevete/Head1"
 
+const rootCss = css({
+  display: "flex",
+  width: 990,
+  flexDirection: 'column',
+  justifyContent: "center",
+  margin: "auto",
+  boxSizing: "border-box",
+  alignItems: "center",
+})
+
+const postsCss = css({
+  display: "flex",
+  boxSizing: "border-box",
+  marginTop: "5em",
+})
+
+const postCss = css({
+  margin: ".5em 1em"
+})
 
 const EventPage: React.FC<PageProps> = ({ data }) => {
 
@@ -12,9 +33,16 @@ const EventPage: React.FC<PageProps> = ({ data }) => {
 
     return (
         <Common>
-            {posts?.map(post => (
-                <PostCard key={post.contentful_id} title={post.title} />
-            ))}
+          <div css={rootCss}>
+            <Head1 text="投稿"/>
+            <div css={postsCss}>
+              {posts?.map(post => (
+                <div key={post.contentful_id} css={postCss}>
+                  <PostCard title={post.title}/>
+                </div>
+              ))}
+            </div>
+          </div>
         </Common>
     )
 }
