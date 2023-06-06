11cf9704d642f4c9e12020d0348f5df8d716c512
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2023-06-06 12:12:32 +0900

    editing description (not finished)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,39 @@
+import { css } from '@emotion/react'
 import * as React from "react"
 import { HeadFC, Link, PageProps, graphql } from "gatsby"
 import { Common } from "../components/common"
+import { Head1 } from "../stories/atrevete/Head1"
 
+const rootStyle = css({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+})
+const textStyle = css({
+    width: '70%',
+    marginTop: '2em',
+    fontSize: '1.3em',
+    fontWeight: '400',
+    letterSpacing: '0.1em',
+    textAlign: 'center'
+})
 
 const Aboutage: React.FC<PageProps> = () => {
 
     return (
         <Common>
+            <div css={rootStyle}>
+            <div css={{textAlign: 'center'}}><Head1 text="Atreveteとは"/></div>
+
+            <p css={textStyle}>
+                Atreveteは、学生を中心としたイベントを開催します。<br/>
+                日本の課題でもある<br/>
+                「若者の育成をどうしていくか」<br/>
+                に着目し、夢ある者、迷いながらも進もうとする者、何をしたらいいかわからない者をサポートします。
+            </p>
+            </div>
         </Common>
     )
 }
