09b4cfb251539c7e0847acf30cdc716c3406f908
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2023-06-08 00:49:13 +0900

    add IROIRO

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,6 +4,7 @@ import { HeadFC, Link, PageProps, graphql } from "gatsby"
 import { Common } from "../components/common"
 import { Head1 } from "../stories/atrevete/Head1"
 import { Html_Head } from '../components/html-head'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const rootStyle = css({
     display: 'flex',
@@ -12,14 +13,37 @@ const rootStyle = css({
     alignItems: 'center',
     width: '100%',
 })
-const textStyle = css({
-    width: '70%',
-    marginTop: '2em',
-    fontSize: '1.3em',
+
+const Wrap = css({
+    margin: '10em 0em',
+    width: '90%',
+    maxWidth: '1200px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+})
+
+const topText = css({
+    width: '60%',
+    fontSize: '2.2em',
     fontWeight: '400',
     letterSpacing: '0.1em',
+    textAlign: 'center',
+})
+
+const messageText = css({
+    marginBottom: '10em',
+    fontWeight: 300,
+    width: '90%',
+    maxWidth: '660px',
+    lineHeight: '40px',
+    fontSize: '20px',
+    alignItems: 'center',
     textAlign: 'center'
 })
+
+
 
 const Aboutage: React.FC<PageProps> = () => {
 
@@ -27,14 +51,22 @@ const Aboutage: React.FC<PageProps> = () => {
         <Common>
             <div css={rootStyle}>
             <div css={{textAlign: 'center'}}><Head1 text="Atreveteとは"/></div>
-
-            <p css={textStyle}>
-                Atreveteは、学生を中心としたイベントを開催します。<br/>
-                日本の課題でもある<br/>
-                「若者の育成をどうしていくか」<br/>
-                に着目し、夢ある者、迷いながらも進もうとする者、何をしたらいいかわからない者をサポートします。
-            </p>
+            <div css={Wrap}>
+            <p css={topText}>私たちは人と人を繋ぎ、<br />特別な価値を提供します。</p>
+            <StaticImage src="../images/top.jpg" css={{width: '40%'}} alt={''} />
             </div>
+            <p css={messageText}>日本の課題でもある「若者の育成をどうしていくか」に着目し、<br/>
+            夢ある者、迷いながらも進もうとする者、何をしたらいいかわからない者をサポートします。<br/>
+            迷いがある者は、迷いを確信へ。<br/>
+            何も決まってない者は、話を聞き少しでも視野を広げます。<br/>
+            希望に満ち溢れる若者が世の中の理不尽に振り回されず、自分自身が主体でいれるように全身全霊で「人　×　人」の場所を提供します。</p>
+            </div>
+            <div css={{textAlign: 'center'}}><Head1 text="ミッション"/></div>
+                <p css={{margin: '3em', textAlign: 'center',fontSize: '30px'}}>会社がどのような像を描いているのかを一言で</p>
+                <div css={Wrap}>
+                    <StaticImage src='../images/top.jpg' css={{width: '40%'}} alt={''}/>
+                    <p css={{width: '60%'}}>具体的に何をしているかなど詳しい説明をつらつら…</p>
+                </div>
         </Common>
     )
 }
