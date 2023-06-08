f92f8d87988ad342d6ec009c33edfbfec794409d
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2023-06-08 13:41:11 +0900

    add markdown package

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -104,6 +104,15 @@ const config: GatsbyConfig = {
       }
     }
   },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `markdown`,
+      path: `${__dirname}/src/markdown`,
+    },
+    
+  },
+  `gatsby-transformer-remark`,
 ]
 };
 
