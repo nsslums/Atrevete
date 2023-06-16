afca06adea7f514fb407ee5ba548d2a2a4ae241c
pantyetta <email@pantyetta.com>
2023-06-16 12:58:49 +0900

    recaptcha defer true

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -5,7 +5,12 @@ import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 export const wrapRootElement = ({ element }) => {
     const key:string = process.env.GATSBY_RECAPTCHA_SITEKEY || ""
     return (
-        <GoogleReCaptchaProvider reCaptchaKey={key}>
+        <GoogleReCaptchaProvider reCaptchaKey={key} scriptProps={{
+            async: false, // optional, default to false,
+            defer: true, // optional, default to false
+            appendTo: 'head', // optional, default to "head", can be "head" or "body",
+            nonce: undefined // optional, default undefined
+          }}>
             {element}
         </GoogleReCaptchaProvider>
     )
