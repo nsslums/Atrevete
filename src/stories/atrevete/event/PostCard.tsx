d75277b2cc60f870322d1272abe9553f2712b59b
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2023-08-02 00:20:20 +0900

    update post card design

--- src/stories/atrevete/event/PostCard.tsx
+++ src/stories/atrevete/event/PostCard.tsx
@@ -7,14 +7,15 @@ const rootCss = css({
     position: "relative",
     width: 320,
     height: 200,
-    padding: 10,
+    // padding: 10,
     border: '1px solid',
     borderTopColor: 'rgba(255,255,255,0.25)',
     borderLeftColor: 'rgba(255,255,255,0.25)',
     borderRightColor: 'rgba(255,255,255,0.1)',
     borderBottomColor: 'rgba(255,255,255,0.1)',
     boxShadow: '0 0 20px rgba(0,0,0,0.5)',
-    borderRadius: '5px',
+    borderRadius: 5,
+    overflow: 'hidden',
     "&:hover": {
       border: "solid 1px rgba(255,2555,255,0.7)",
       "&:active": {
@@ -33,6 +34,15 @@ const imageCss = css({
     width: "100%",
     height: "100%",
     objectFit: "cover",
+    "&:after": {
+      content: '""',
+      background: 'linear-gradient(rgba(0,0,0,0),rgba(0,0,0,1))',
+      position: 'absolute',
+      width: '100%',
+      height: 70,
+      left: 0,
+      bottom: 0,
+    }
 })
 
 const textCss = css({
@@ -41,7 +51,10 @@ const textCss = css({
     bottom: 0,
     textAlign: 'center',
     fontSize: '20px',
-    marginBottom: "1em"
+    marginBottom: "1em",
+    "> p": {
+      textShadow: "0 0 5px black",
+    }
 })
 
 interface EventCardProps {
@@ -61,11 +74,11 @@ export const PostCard = ({
   return (
     <div css={rootCss}>
       <Link to={url} css={linkCss} draggable={false}>
-        <div css={{height:"70%"}}>
+        <div css={{height:"100%"}}>
             {image ? 
               <GatsbyImage css={imageCss} image={image} alt='eyecatch'/>
             :
-              <StaticImage src='../../../images/noimage.png' css={imageCss} alt='eyecatch'/>
+              <StaticImage src='../../../../static/noimg.png' css={imageCss} alt='eyecatch'/>
             }
         </div>
         <div css={textCss}>
