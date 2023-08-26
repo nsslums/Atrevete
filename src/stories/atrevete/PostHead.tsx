8239819f7f5dc90f28ef65a0c5ae80aa72786099
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2023-08-27 01:16:31 +0900

    fix image ratio

--- src/stories/atrevete/PostHead.tsx
+++ src/stories/atrevete/PostHead.tsx
@@ -12,14 +12,17 @@ interface PostHeadProps{
 }
 
 const eyeCatch = css({
-    margin: '2em 0',
-    height: "200px",
+    margin: '1em 2em',
+    // height: "200px",
+    // width: "100%",
 })
 
 const imageCss = css({
     height: "100%",
     width: "100%",
     objectFit: "cover",
+    borderRadius: "4px",
+    boxShadow: "0 0 4px rgba(0, 0, 0, .4)",
 })
 
 const rootCss = css({
